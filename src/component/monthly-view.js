1ee2631743b7756d20be80237a7d5e3e1da36571
reza seddighi <r@r.com>
2021-07-15 11:18:05 +0430

    initialization

---
+++ src/component/monthly-view.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./monthly-view.module.css";
+
+export default function MonthlyView() {
+  return <section className={styles.monthlyview}></section>;
+}
